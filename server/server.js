d96cd45a7701ee0a11e224cb8a8a7d570dd77d71
Uday Nayak <udaynayak6037@gmail.com>
2025-10-29 15:26:39 +0530

    feat(server): initialize server with Express and basic configuration
    - Added package.json for server dependencies and scripts
    - Created server.js to set up Express app and HTTP server
    - Implemented middleware for JSON parsing and CORS
    - Added a simple API endpoint to check server status

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import "dotenv/config"
+import cors from 'cors'
+import http from 'http'
+
+// create experss app and HTTP
+const app = express(); 
+const server= http.createServer(app)
+//middlesware
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+app.use("/api/status",(req,res)=> res.send("Server is live"));
+const PORT =process.env.PORT || 5000;
+server.listen(PORT, ()=> console.log("Server is listening on port:"+PORT));
